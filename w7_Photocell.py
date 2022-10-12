9c8273a0643145fa3cb2cdb47eed1fac2e6d9ea6
LiuliuLexie <99244703+LiuliuLexie@users.noreply.github.com>
2022-10-12 12:09:04 +0400

    Update w7_Photocell.py

--- w7_Photocell.py
+++ w7_Photocell.py
@@ -15,12 +15,22 @@ photocell = analogio.AnalogIn(board.A1)
 def analog_voltage(adc):
     return adc.value / 65535 * adc.reference_voltage
 
+darkThreshold = 20000
+brightThreshold = 60000
+
 # Main loop reads value and voltage every second and prints them out.
 while True:
     # Read the value, then the voltage.
     val = photocell.value
     volts = analog_voltage(photocell)
     # Print the values:
-    print('Photocell value: {0} voltage: {1}V'.format(val, volts))
+
+    if(val > brightThreshold):
+        print('Photocell value: {0} bright voltage: {1}V'.format(val, volts))
+    if(val < darkThreshold):
+        print('Photocell value: {0} dark voltage: {1}V'.format(val, volts))
+    else:
+        print('Photocell value: {0} voltage: {1}V'.format(val, volts))
     # Delay for a second and repeat!
-    time.sleep(1.0)
+    time.sleep(2.0)
+
